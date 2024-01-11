f8324ff971add1635dd5b32f5ca916a130923e59
Ömer Faruk PALA <101570820+omerfarukkpala@users.noreply.github.com>
2024-01-12 00:01:19 +0300

    Update Program.cs
    ...

--- Koleksiyonlar-Soru-1/Program.cs
+++ Koleksiyonlar-Soru-1/Program.cs
@@ -6,13 +6,6 @@ namespace Collections
 {
     class Program
     {
-        /* 
-
-        Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
-        Negatif ve numeric olmayan girişleri engelleyin.
-        Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
-        Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
-        */
         static bool IsPrimeNumber(int sayi)// islemler arasında → asal olup olmadığını işaretlemek için
         {
             if (sayi <= 1) // asal sayılar ikiden başlar en küçük asal sayı 2 dir.
